71e8c2f0102c1e004cc904260b968febbc7d3b3a
frekerzzzz <kannamugesh135@gmail.com>
2023-12-22 15:21:43 +0530

    origin

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ var nodemailer = require("nodemailer");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin:"*",
+  }
+));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
