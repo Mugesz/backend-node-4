f7821cc5e0eea4e22097f3738b1edee84bde504e
frekerzzzz <kannamugesh135@gmail.com>
2023-12-22 15:31:55 +0530

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors(
 ));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin","https://master--zingy-pony-c1800a.netlify.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
