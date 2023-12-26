91b1b0844fedb3357c4ef8a1f4b5acfc13d729ff
frekerzzzz <kannamugesh135@gmail.com>
2023-12-26 18:43:35 +0530

    cors

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin:"https://master--zingy-pony-c1800a.netlify.app",
+    origin:"https://zingy-pony-c1800a.netlify.app/",
   }
 ));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin","https://master--zingy-pony-c1800a.netlify.app");
+  res.header("Access-Control-Allow-Origin","https://zingy-pony-c1800a.netlify.app/");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
